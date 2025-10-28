f5bee1441c2249c94d28eee6c8f9d2a0585dc1fd
Kevin Gege <kcampodonico@unprg.edu.pe>
2025-10-28 18:20:29 -0500

    feat(auth): Add SignUpRequest entity for user registration

---
+++ lib/domain/entities/signup_params.dart
@@ -0,0 +1,11 @@
+class SignUpRequest {
+  final String email;
+  final String password;
+  final String username;
+
+  SignUpRequest({
+    required this.email,
+    required this.password,
+    required this.username,
+  });
+}
